855451c653bb5a780b09c67f7e6187f9040e2e16
TranDangKhoaID <trankhoa26102002@gmail.com>
2023-09-10 16:34:46 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,116 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:social_media_app/components/text_field.dart';
+import 'package:social_media_app/components/wall_post.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //user
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  //text controller
+  final textController = TextEditingController();
+  //log out
+  void signOut(){
+    FirebaseAuth.instance.signOut();
+  }
+  //post message
+  void postMessage(){
+    if (textController.text.isNotEmpty) {
+      FirebaseFirestore.instance.collection("User Posts").add({
+        'UserEmail': currentUser.email,
+        'Message': textController.text,
+        'TimeStamp': Timestamp.now(),
+      });
+    }
+
+    //clear the text
+    setState(() {
+      textController.clear();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: const Text('The Wall'),
+        actions: [
+          IconButton(
+            onPressed: signOut, 
+            icon: const Icon(Icons.logout)
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          //the wall
+          Expanded(
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("User Posts")
+                  .orderBy(
+                    "TimeStamp",
+                    descending: true
+                  )
+                  .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          //get the message
+                          final post = snapshot.data!.docs[index];
+                          return WallPost(
+                            message: post['Message'], 
+                            user: post['UserEmail'], 
+                          ); 
+                        },
+                      );
+                    } else if(snapshot.hasError){
+                      return Center(
+                        child: Text('Error: ${snapshot.error}'),
+                      );
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+            )
+          ),
+          //post message
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: MyTextField(
+                    controller: textController,
+                    hintText: 'Write something on the wall',
+                    obscureText: false,
+                  )
+                ),
+                //post button
+                IconButton(
+                  onPressed: postMessage, 
+                  icon: const Icon(Icons.arrow_circle_up)
+                )
+              ],
+            ),
+          ),
+          //logged in as
+          Text("Logged in as: " + currentUser.email!, style: const TextStyle(color: Colors.grey),),
+          const SizedBox(height: 30,)
+        ],
+      )
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,144 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:social_media_app/components/button.dart';
+import 'package:social_media_app/components/text_field.dart';
+
+class RegisterPage extends StatefulWidget {
+  final Function()? onTap;
+  const RegisterPage({super.key, required this.onTap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+ 
+  //text editing controller
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+  final confirmPasswordTextController = TextEditingController();
+  //sign up
+  void signUp() async {
+    //show dia log
+    showDialog(
+      context: context, 
+      builder: (context) => const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+    //make sure password match
+    if (passwordTextController.text != confirmPasswordTextController.text) {
+      //pop loadinh circle
+      Navigator.pop(context);
+      //show error
+      displayMessage("Passwords don't match!");
+    }
+    //try creating the user
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailTextController.text, 
+        password: passwordTextController.text);
+      //
+      if (context.mounted) {
+        Navigator.pop(context);
+      }
+    } on FirebaseAuthException catch (e) {
+      //
+      Navigator.pop(context);
+      displayMessage(e.code);
+    }
+  }
+  //display a dialog message
+  void displayMessage(String message){
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: Text(message),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //logo
+                  const Icon(
+                    Icons.lock,
+                    size: 100,
+                  ),
+                  const SizedBox(height: 40,),
+                  //welcome
+                  Text(
+                    "Lets create an account for you",
+                    style: TextStyle(
+                      color: Colors.grey[700]
+                    ),
+                  ),
+                  const SizedBox(height: 25,),
+                  //email textfield
+                  MyTextField(
+                    controller: emailTextController, 
+                    hintText: 'Email', 
+                    obscureText: false
+                  ),
+                  const SizedBox(height: 10,),
+                  //password textfield
+                  MyTextField(
+                    controller: passwordTextController, 
+                    hintText: 'Password', 
+                    obscureText: true
+                  ),
+                  const SizedBox(height: 10,),
+                  //confirm password textfield
+                  MyTextField(
+                    controller: confirmPasswordTextController, 
+                    hintText: 'Confirm Password', 
+                    obscureText: true
+                  ),
+                  const SizedBox(height: 10,),
+                  //sign in button
+                  MyButton(
+                    onTap: signUp,
+                    text: 'Sign Up'
+                  ),
+                  const SizedBox(height: 25,),
+                  //go to register
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          color: Colors.grey[700]
+                        ),
+                      ),
+                      const SizedBox(width: 4,),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: const Text(
+                          "Login here",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
