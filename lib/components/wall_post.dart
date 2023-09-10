936f451d70bf360881e63a7480cda78b01d3bbb1
TranDangKhoaID <trankhoa26102002@gmail.com>
2023-09-10 17:16:40 +0700

    add like post

--- lib/components/wall_post.dart
+++ lib/components/wall_post.dart
@@ -1,9 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_app/components/like_button.dart';
 
-class WallPost extends StatelessWidget {
+class WallPost extends StatefulWidget {
   final String message;
   final String user;
-  const WallPost({super.key, required this.message, required this.user});
+  final String postId;
+  final List<String> likes;
+
+  const WallPost({
+    super.key, 
+    required this.message, 
+    required this.user, 
+    required this.postId, 
+    required this.likes
+  });
+
+  @override
+  State<WallPost> createState() => _WallPostState();
+}
+
+class _WallPostState extends State<WallPost> {
+
+  //user
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  bool isLiked = false;
+
+  @override
+  void initState() {  
+    super.initState();
+    isLiked = widget.likes.contains(currentUser.email);
+  }
+
+  //toggle like
+  void toggleLike(){
+    setState(() {
+      isLiked = !isLiked;
+    });
+
+    ///access the document is firebase
+    DocumentReference posRef = FirebaseFirestore.instance.collection('User Posts').doc(widget.postId);
+    if (isLiked) {
+      posRef.update({
+        'Likes' : FieldValue.arrayUnion([currentUser.email])
+      });
+    }else{
+      posRef.update({
+        'Likes': FieldValue.arrayRemove([currentUser.email])
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +61,17 @@ class WallPost extends StatelessWidget {
       padding: const EdgeInsets.all(25),
       child: Row(
         children: [
-          //image
-          Container(
-            decoration:
-                BoxDecoration(shape: BoxShape.circle, color: Colors.grey[400]),
-            padding: const EdgeInsets.all(10),
-            child: const Icon(Icons.person, color: Colors.white,),
+          Column(
+            children: [
+              //like button
+              LikeButton(
+                isLiked: isLiked, 
+                onTap: toggleLike
+              ),
+              const SizedBox(height: 5,),
+              //like count
+              Text(widget.likes.length.toString(), style: const TextStyle(color: Colors.grey),),
+            ],
           ),
           const SizedBox(width: 20,),
           Column(
@@ -27,13 +79,13 @@ class WallPost extends StatelessWidget {
             children: [
               //
               Text(
-                user,
+                widget.user,
                 style: TextStyle(color: Colors.grey[500]),
               ),
               const SizedBox(
                 height: 10,
               ),
-              Text(message),
+              Text(widget.message),
             ],
           )
         ],
