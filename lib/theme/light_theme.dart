879f4ad86dec8fdb4255b861958131b3151f8f86
TranDangKhoaID <trankhoa26102002@gmail.com>
2023-09-11 20:19:26 +0700

    add delete my posts

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -6,11 +6,14 @@ ThemeData lightTheme = ThemeData(
     backgroundColor: Colors.transparent,
     elevation: 0,
     iconTheme: IconThemeData(color: Colors.black),
-    titleTextStyle: TextStyle(color: Colors.black, fontSize: 20)
+    titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
   ),
   colorScheme: ColorScheme.light(
     background: Colors.grey[300]!,
     primary: Colors.grey[200]!,
-    secondary: Colors.grey[300]!
-  )
+    secondary: Colors.grey[300]!,
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(foregroundColor: Colors.black),
+  ),
 );
